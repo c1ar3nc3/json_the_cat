8d881f9d778d5a98451c303543de9766c7e9361f
Clarence Williams <xwilliams.clarencex@gmail.com>
2020-12-03 21:55:41 +0000

    w2d4

---
+++ breedFetcher.js
@@ -0,0 +1,21 @@
+const request = require('request');
+
+//endpoint -> https://api.thecatapi.com/v1/breeds/search.
+
+const options = process.argv[2];
+
+const breedfetcher = (options, callback) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${options}`, (err, response, body) => {
+    const data = JSON.parse(body);
+    if (err) {
+      callback(err, null);
+    } else if (data[0] === undefined) {
+      console.log("Error: Cat not found");
+    } else {
+      const description = data[0].description;
+      console.log(description);
+    }
+  });
+};
+
+breedfetcher("Siberian");
